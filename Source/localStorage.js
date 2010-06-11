dcb867d5faea8c0e51ec8f10583cdd9f9a7013bd
Henrik Hansen <henrikb4@gmail.com>
2010-06-11 23:36:15 +0200

    Added basic support for localStorage

---
+++ Source/localStorage.js
@@ -0,0 +1,43 @@
+/*
+---
+
+script: localStorage.js
+
+description: Persisten storage using localStorage
+
+license: MIT-style license.
+
+authors: Henrik Enggaard Hansen
+
+requires:
+- DataStorage
+
+provides: [Adapter.localStorage]
+
+...
+*/
+
+Adapter.localStorage = new Class({
+	initialize: function(model){
+		if (!("_id" in model.scheme)){
+			model.scheme["_id"] = "";
+		}
+	},
+	save: function(record, callback){
+		if (record.data["_id"] === "") {
+			record.data["_id"] = uuid();
+		}
+		localStorage[record.model.name + "::" + record.data["_id"]] = JSON.encode(record.data);
+		callback();
+	},
+	destroy: function(record, callback){
+		delete this.database[record.model.name + "::" + record.data["_id"]]
+		callback();
+	},
+	filter: function(){
+		
+	},
+	find: function(){
+		
+	}
+});
